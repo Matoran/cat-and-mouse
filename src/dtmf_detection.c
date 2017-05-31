e67c4834796a9cf497ca5a1cee93c47ce5ddb77b
Exwgaet <exwgaet@gmail.com>
2017-05-31 13:39:33 +0200

    Add dtmf detection + basic cat

--- src/dtmf_detection.c
+++ src/dtmf_detection.c
@@ -7,20 +7,59 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
+#include <queue.h>
 #include "adc.h"
 #include "debug.h"
 #include "objects.h"
+#include "dtmf_detection.h"
 
 #define BUF_SIZE 400
+#define LIMIT_DETEC 120000
 #define FE 8000.
 #define PI 3.14159265359
+#define abs1(x) ((x<0)?(-x):x)
 
+QueueHandle_t xQueue;
 
+bool check_freq(uint16_t* buf, uint16_t freq){
+	double w;
+	complex sum;
+	sum.im = 0;
+	sum.re = 0;
+
+	for (int i = 0; i < BUF_SIZE - 1; ++i) {
+		w = (2*PI*(852)*i)*PI/180;
+		sum.im += buf[i] * sinf(w-BUF_SIZE);
+		sum.re += buf[i] * cosf(w-BUF_SIZE);
+	}
+
+	if ((abs1(sum.im)+abs1(sum.re)) > LIMIT_DETEC){
+		return true;
+	}else{
+		return false;
+	}
+}
 
 // if you need additional buffers, enable the next line and declare them after it to place them in local RAM (RAM2 could be full!)
 //__attribute__ ((section(".data")))
+int direction (uint16_t* buf){
+	if (check_freq(buf,852)) {
+		if (check_freq(buf,1336)) {
+			return NORTH;
+		}else if (check_freq(buf,1608)){
+			return EAST;
+		}
+	}else if (check_freq(buf,941)) {
+		if (check_freq(buf,1336)) {
+			return SOUTH;
+		}else if (check_freq(buf,1608)){
+			return WEST;
+		}
+	}
 
-
+	return -1;
+}
 
 
 
@@ -28,17 +67,52 @@
 // buf_index: 0 or 1, indicating which buffer is full
 void buffer_filled(int buf_index)
 {
-
+	xQueueSendToBack(xQeue, &buf_index, portMAX_DELAY);
 }
 
 // Initialise the reception of the sound samples
 void init_dtmf()
 {
-	unsigned short *sig;       // pointer on acquisition double buffer
+	unsigned short *sig; // pointer on acquisition double buffer
 	// initialise DMA&ADC to receive samples from the audio line at 8000 kHz.
 	// The return pointer points on the the double buffer allocated.
 	if ((sig=init_adc_dma(1, FE, BUF_SIZE, buffer_filled))==NULL)
 		EXIT("Not enough memory to allocate acquisition buffers!");
 }
 
+void init_cat(){
+	cat_t cat;
+	cat.posX = 110;
+	cat.oldPosX = cat.posX;
+	cat.posY = 30;
+	cat.oldPoxY = cat.posY;
+	cat.direction = SOUTH;
+}
 
+void cat_move(){
+	if (xQueue = xQueueCreate( 1, sizeof(int)) == NULL) {
+		EXIT("Fail to create DMA queue !");
+	};
+
+	init_dtmf();
+	init_cat();
+
+	int pos;
+	int buf;
+	unsigned short *buf1 = *sig;
+	unsigned short *buf2 = *sig + BUF_SIZE;
+
+	while(1){
+
+		xQeueReceive(xQeue, &buf,portMAX_DELAY);
+
+		if (buf == 0) {
+			pos = direction(buf1);
+		}else if (buf == 1){
+			 pos = direction(buf2);
+		}
+
+		xQueueSendToBack(catQueue, &cat, portMAX_DELAY);
+
+	}
+}
--- src/dtmf_detection.h
+++ src/dtmf_detection.h
@@ -2,6 +2,26 @@
 #define DTMF_DETECTION_H
 
 #include "objects.h"
+#include "define.h"
+
+typedef struct complex{
+	float re;
+	float im;
+}complex;
+
+typedef struct cat_t{
+	int posX;
+	int posY;
+	int direction;
+	int oldPosX;
+	int oldPoxY;
+}cat_t;
+
+typedef enum Direction Direction;
+enum Direction
+{
+    NORTH, EAST, SOUTH,WEST
+};
 
 void init_dtmf();
 dir_t read_dtmf(int buf_idx);
