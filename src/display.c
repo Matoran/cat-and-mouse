1602e1dcb1b118679071fcd884149434b0055f34
Matoran <Matoran@users.noreply.github.com>
2017-05-31 21:50:25 +0200

    edit vitality gauge and interface

--- src/display.c
+++ src/display.c
@@ -2,7 +2,7 @@
 #include "mouse.h"
 
 
-void draw_vitality(int vitality){
+void draw_interface(int vitality){
 	static int lastVitality = VITALITYMAX;
 	if(lastVitality > vitality){
 		lcd_filled_rectangle(220, 318-mouse.vitality*20/VITALITYMAX, 231, (318-mouse.vitality*20/75)+1, LCD_WHITE);
@@ -11,6 +11,13 @@ void draw_vitality(int vitality){
 	}
 	lastVitality = vitality;
 
+	for (i=0; i<3; i++)
+	{
+		display_bitmap16(sprites.cheese_im.bitmap, 28+80*i, 0, sprites.cheese_im.width, sprites.cheese_im.height);
+		lcd_filled_rectangle(30+80*i, 313, 30+12+80*i, 319, LCD_BLACK);		// mouse pits
+		lcd_filled_circle(30+6+80*i, 313, 6, LCD_BLACK);
+	}
+	lcd_empty_rectangle(219, 299, 233, 319, LCD_BLACK);						// empty gauge
 }
 
 void display(){
@@ -18,9 +25,8 @@ void display(){
 	draw_vitality(mouse.vitality);
 	draw_mouse(mouse);
 	draw_cat(cat);
-	if(mouse touch cheese){
+	if(detect_collision){
 
 	}
-	draw_cheeses();
-	draw_pits();
+	draw_interface();
 }
